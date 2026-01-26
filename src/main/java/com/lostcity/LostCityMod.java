1e90ecaee9f94c391e0ec13f337eb4387968ef20
sunshainy <lexalex2017@gmail.com>
2026-01-26 15:06:26 +0500

    Update mod version to 0.6.0-beta and fix maven group/archives name

--- src/main/java/com/lostcity/LostCityMod.java
+++ src/main/java/com/lostcity/LostCityMod.java
@@ -24,7 +24,7 @@ public class LostCityMod implements ModInitializer {
 
     public static final String MOD_ID = "lostcities";
     public static final String MOD_NAME = "Lost City";
-    public static final String VERSION = "1.0.0-SNAPSHOT";
+    public static final String VERSION = "0.6.0-beta";
 
     /** Логгер для всего мода */
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_NAME);
